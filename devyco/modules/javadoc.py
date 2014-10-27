f913165e1d134c6968b025a06876049b108164e8
Henrik Stråth <henrik@yubico.com>
2014-10-27 15:28:59 +0100

    javadoc module RC1

--- devyco/modules/javadoc.py
+++ devyco/modules/javadoc.py
@@ -1,74 +1,73 @@
 """
-Clones a git repo and extracts data.
+Fetches JavaDoc and publishes from Maven Central.
 Activated by a "javadoc" entry in .conf.json, containing the following settings:
-    url: Git repository URL (required).
-    files: List of filepatterns to copy from the repository (default: all)
+    groupId: The Maven groupId for the project
+    artifactId: The Maven artifactId for the project
 """
 
-import os
+import shutil
+from os import path
 from urllib2 import urlopen
 from StringIO import StringIO
 from zipfile import ZipFile
 from devyco.module import Module
 
-JAVADOC_URL = ('http://repository.sonatype.org/service/local/artifact/maven/'
+LAST_SEEN_HASH = 'last_seen_hash'
+
+JAVADOC_ARCHIVE_URL = ('http://repository.sonatype.org/service/local/artifact/maven/'
                'redirect?r=central-proxy'
-               '&g=%(group)s'
-               '&a=%(artifact)s'
+               '&g={group}'
+               '&a={artifact}'
                '&v=LATEST&c=javadoc')
 
 HASH_URL = ('https://repo1.maven.org/maven2/'
-            '%(group_url)s/'
-            '%(artifact)s/'
-            '/maven-metadata.xml.md5')
-
-def _up_to_date(group, artifact):
-    latest_seen_hash = _get_last_seen_hash()
-    print 'latestSeen:'
-    print latest_seen_hash
-    remote_hash = _get_remote_hash(group, artifact)
-    print remote_hash
-    with open('.last_seen_hash', 'w') as latest_seen_hash:
-        latest_seen_hash.write(remote_hash)
-    return remote_hash == latest_seen_hash
+            '{group_url}/{artifact}/'
+            'maven-metadata.xml.md5')
 
 
-def _get_last_seen_hash():
-    if not os.path.exists('.last_seen_hash'):
+def _get_last_seen_hash(hash_path):
+    if not path.exists(hash_path):
         return None
-    with open('.last_seen_hash', 'r') as latest_seen_hash:
-        return latest_seen_hash.read()
-
-
-def _get_remote_hash(group, artifact):
-    return urlopen(HASH_URL.format(group=group.replace('.', '/'),
-                                           artifact=artifact)).read()
+    with open(hash_path, 'r') as hash_file:
+        return hash_file.read()
 
 
 class JavaDocModule(Module):
     def __init__(self):
         super(JavaDocModule, self).__init__()
-        self._updated = []
 
     def _run(self):
         conf = self.get_conf('javadoc')
         if conf is None:
             return
 
-        group = conf['groupId']
-        artifact = conf['artifactId']
+        self.group = conf['groupId']
+        self.artifact = conf['artifactId']
+        self.cache_root = self.cache_dir(self.group + self.artifact, True)
 
-        print 'JavaDoc module!!!'
-        print group
-        print artifact
+        javadoc_cache_path = path.join(self.cache_root, 'JavaDoc')
 
-        if _up_to_date(group, artifact):
-            return
+        if not self._up_to_date() or not path.exists(javadoc_cache_path):
+            url = JAVADOC_ARCHIVE_URL.format(group=self.group,
+                                             artifact=self.artifact)
+            jarfile = urlopen(url).read()
+            zipfile = ZipFile(StringIO(jarfile))
+            zipfile.extractall(javadoc_cache_path)
 
-        url = JAVADOC_URL.format(group=group, artifact=artifact)
-        jarfile = url.read(urlopen(url))
-        zipfile = ZipFile(StringIO(jarfile))
-        zipfile.extractall('JavaDoc')
+        shutil.copytree(javadoc_cache_path, path.join(self._target, 'JavaDoc'))
+
+    def _up_to_date(self):
+        hash_path = path.join(self.cache_root, LAST_SEEN_HASH)
+        last_seen_hash = _get_last_seen_hash(hash_path)
+        remote_hash = self._get_remote_hash()
+        with open(hash_path, 'w') as hash_file:
+            hash_file.write(remote_hash)
+        return remote_hash == last_seen_hash
+
+    def _get_remote_hash(self):
+        url = HASH_URL.format(group_url=self.group.replace('.', '/'),
+                              artifact=self.artifact)
+        return urlopen(url).read()
 
 
 module = JavaDocModule()
