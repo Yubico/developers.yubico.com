1111070d94d139d225de625a2780f96d7ebef983
Henrik Stråth <henrik.strath@gmail.com>
2014-10-27 10:24:54 +0100

    Refactored javadoc module
    To make it more Python and less Java

--- devyco/modules/javadoc.py
+++ devyco/modules/javadoc.py
@@ -1,11 +1,8 @@
 """
 Clones a git repo and extracts data.
-Activated by a "git" entry in .conf.json, containing the following settings:
+Activated by a "javadoc" entry in .conf.json, containing the following settings:
     url: Git repository URL (required).
     files: List of filepatterns to copy from the repository (default: all)
-
-The "git" entry can also be a list of objects containing the settings above if
-multiple repositories should be clones.
 """
 
 import os
@@ -15,9 +12,41 @@ from os import path
 from glob import glob
 from devyco.module import Module
 
+JAVADOC_URL = ('http://repository.sonatype.org/service/local/artifact/maven/'
+               'redirect?r=central-proxy'
+               '&g=%(group)s'
+               '&a=%(artifact)s'
+               '&v=LATEST&c=javadoc')
+
+HASH_URL = ('https://repo1.maven.org/maven2/'
+            '%(group_url)s/'
+            '%(artifact)s/'
+            '/maven-metadata.xml.md5')
+
+def _up_to_date(group, artifact):
+    latest_seen_hash = _get_last_seen_hash()
+    print 'latestSeen:'
+    print latest_seen_hash
+    remote_hash = _get_remote_hash(group, artifact)
+    print remote_hash
+    with open('.last_seen_hash', 'w') as latest_seen_hash:
+        latest_seen_hash.write(remote_hash)
+    return remote_hash == latest_seen_hash
+
+
+def _get_last_seen_hash():
+    if not os.path.exists('.last_seen_hash'):
+        return None
+    with open('.last_seen_hash', 'r') as latest_seen_hash:
+        return latest_seen_hash.read()
+
+
+def _get_remote_hash(group, artifact):
+    return urllib2.urlopen(HASH_URL.format(group=group.replace('.', '/'),
+                                           artifact=artifact)).read()
+
 
 class JavaDocModule(Module):
-
     def __init__(self):
         super(JavaDocModule, self).__init__()
         self._updated = []
@@ -27,35 +56,17 @@ class JavaDocModule(Module):
         if conf is None:
             return
 
-        self.group = conf['groupId']
-        self.artifact = conf['artifactId']
+        group = conf['groupId']
+        artifact = conf['artifactId']
 
         print 'JavaDoc module!!!'
-        print self.group
-        print self.artifact
+        print group
+        print artifact
 
-        latestSeenHash = self._getLastSeenHash()
-        
-        print 'latestSeen:'
-        print latestSeenHash
+        if _up_to_date(group, artifact):
+            return
 
-        remoteHash = self._getRemoteHash()
-        print remoteHash
-        
-        with open('.last_seen_hash', 'w') as latest_seen_hash:
-            latest_seen_hash.write(remoteHash)
-
-
-
-    def _getRemoteHash(self):
-        url = 'https://repo1.maven.org/maven2/' + self.group.replace('.', '/') + '/' + self.artifact + '/maven-metadata.xml.md5'
-        return urllib2.urlopen(url).read()
-
-    def _getLastSeenHash(self):
-        if not os.path.exists('.last_seen_hash'):
-            return None
-        with open('.last_seen_hash', 'r') as latest_seen_hash:
-            return latest_seen_hash.read()
+        url = JAVADOC_URL.format(group=group, artifact=artifact)
 
 
 module = JavaDocModule()
