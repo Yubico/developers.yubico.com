1c8413d902d8f3353fff12469a61d0eb15f0c622
Henrik Stråth <henrik@yubico.com>
2015-09-09 18:21:30 +0200

    Muted URLErrors from the JavaDoc module, until we've gotten 10 consecutive errrors.

--- devyco/modules/javadoc.py
+++ devyco/modules/javadoc.py
@@ -8,13 +8,16 @@ Activated by a "javadoc" entry in .conf.json, containing the following settings:
 import os
 import shutil
 from os import path
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from StringIO import StringIO
 from zipfile import ZipFile
 from devyco.module import Module
 from xml.dom import minidom
 
+MAX_CONSECUTIVE_TIMEOUTS = 10  # Accept occasional timeouts from Maven Central since this part of their API is not very stable.
+
 LOCAL_VERSION = 'local_version'
+CONSECUTIVE_TIMEOUTS = 'consecutive_timeouts'
 
 JAVADOC_ARCHIVE_URL = (
     'http://search.maven.org/remotecontent?'
@@ -26,12 +29,22 @@ HASH_URL = ('https://repo1.maven.org/maven2/'
             '{group_url}/{artifact}/'
             'maven-metadata.xml')
 
+"""
+Stores single values in a file.
+"""
+class FileStore(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-def _get_local_version(version_path):
-    if not path.exists(version_path):
-        return None
-    with open(version_path, 'r') as version_file:
-        return version_file.read()
+    def read(self):
+        if not path.exists(self.file_path):
+            return None
+        with open(self.file_path, 'r') as f:
+            return f.read()
+
+    def write(self, value):
+        with open(self.file_path, 'w') as version_file:
+            version_file.write(str(value))
 
 
 class JavaDocModule(Module):
@@ -44,7 +57,7 @@ class JavaDocModule(Module):
             return
 
         if os.environ.get('OFFLINE'):
-            print "OFFLINE set, skip javadoc"
+            print 'OFFLINE set, skip javadoc'
             return
 
         self.group_url = conf['groupId'].replace('.', '/')
@@ -52,24 +65,36 @@ class JavaDocModule(Module):
         self.cache_root = self.cache_dir(self.group_url + self.artifact, True)
 
         javadoc_cache_path = path.join(self.cache_root, 'JavaDoc')
-        version_path = path.join(self.cache_root, LOCAL_VERSION)
+        version_store = FileStore(path.join(self.cache_root, LOCAL_VERSION))
+        consecutive_timeouts_store = FileStore(path.join(self.cache_root, CONSECUTIVE_TIMEOUTS))
 
-        if not self._up_to_date(version_path) or not path.exists(
-                javadoc_cache_path):
+        try:
+            up_to_date = self._up_to_date(version_store)
+            consecutive_timeouts = 0
+        except URLError as e:
+            consecutive_timeouts = int(
+                consecutive_timeouts_store.read() or 0) + 1
+            if consecutive_timeouts > MAX_CONSECUTIVE_TIMEOUTS:
+                raise IOError(
+                    'Fetching of Maven Central URL has failed all of the last %s builds: %s' % (
+                        consecutive_timeouts, e))
+            up_to_date = True
+        consecutive_timeouts_store.write(consecutive_timeouts)
+
+        if not up_to_date or not path.exists(javadoc_cache_path):
             url = JAVADOC_ARCHIVE_URL.format(group_url=self.group_url,
                                              artifact=self.artifact,
                                              version=self.remote_version)
             jarfile = urlopen(url).read()
             zipfile = ZipFile(StringIO(jarfile))
             zipfile.extractall(javadoc_cache_path)
-            with open(version_path, 'w') as version_file:
-                version_file.write(self.remote_version)
+            version_store.write(self.remote_version)
 
         shutil.copytree(javadoc_cache_path, path.join(self._target, 'JavaDoc'))
 
-    def _up_to_date(self, version_path):
+    def _up_to_date(self, version_store):
         self._get_remote_version()
-        return self.remote_version == _get_local_version(version_path)
+        return self.remote_version == version_store.read()
 
     def _get_remote_version(self):
         url = HASH_URL.format(group_url=self.group_url,
