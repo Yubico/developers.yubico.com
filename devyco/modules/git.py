bb68655faddab16c9ed88c033a90fd1472ae76fa
Dain Nilsson <dain@yubico.com>
2015-12-07 16:08:02 +0100

    Create dynamic rewrites based on past filenames.

--- devyco/modules/git.py
+++ devyco/modules/git.py
@@ -9,7 +9,9 @@ multiple repositories should be clones.
 """
 
 import os
+import re
 import shutil
+import subprocess
 from os import path
 from glob import glob
 from devyco.module import Module
@@ -35,6 +37,7 @@ class GitModule(Module):
         for conf in confs:
             repo_dir = self._clone(conf)
             self._copy_files(repo_dir, conf.get('files', '*'))
+            self._create_redirects(repo_dir, conf.get('redirect_renamed', []))
 
     def _clone(self, conf):
         url = conf['url']
@@ -79,6 +82,59 @@ class GitModule(Module):
                 shutil.copytree(match, path.join(target, path.basename(match)))
             else:
                 shutil.copy(match, target)
+
+    def _create_redirects(self, repo_dir, entries):
+        redirects = []
+        if isinstance(entries, basestring):
+            entries = [entries]
+        for entry in entries:
+            directory = entry.get('dir', '')
+            prefix = path.join(repo_dir, directory)
+            for fpattern in entry.get('files', ['*']):
+                for match in glob(path.join(prefix, fpattern)):
+                    match = match[len(prefix)+1:]
+                    names = self._get_old_names(repo_dir, directory, match)
+                    names = self._substitutions(names, entry.get('sub'))
+                    redirects += self._redirects_for(names)
+        if redirects:
+            print "\n\n\n\n!!!!!!!!REDIRECTS:", redirects
+            htaccess = path.join(self._target, '.htaccess')
+            print "DEST:", htaccess
+            with open(htaccess, 'w') as f:
+                f.writelines(redirects)
+            print "\n\n\n\n"
+
+    def _get_old_names(self, repo_dir, directory, fname):
+        p = subprocess.Popen(['git', 'log', '--name-only', '--oneline', '--follow', fname],
+                             stdout=subprocess.PIPE,
+                             cwd=path.join(repo_dir, directory))
+        (out, err) = p.communicate()
+
+        prefix = path.join(directory, '')
+        lines = [line[len(prefix):] for line in out.splitlines()
+                 if line.startswith(directory)]
+        seen = set()
+        lines = [x for x in lines if x not in seen and not seen.add(x)]
+        return filter(None, lines)
+
+    def _substitutions(self, names, subs):
+        if isinstance(subs, basestring):
+            subs = [subs]
+        for sub in subs:
+            names = [re.sub(sub['pattern'], sub['repl'], name) for name in names]
+        return names
+
+    def _redirects_for(self, names):
+        if len(names) < 2:
+            return []
+
+        redirects = set()
+        basepath = path.join(*self._context['path'])
+        new_path = path.join(basepath, names[0])
+        for old_name in names[1:]:
+            redirects.add("Redirect 301 /%s /%s\n" % (
+                path.join(basepath, old_name), new_path))
+        return list(redirects)
 
 
 module = GitModule()
